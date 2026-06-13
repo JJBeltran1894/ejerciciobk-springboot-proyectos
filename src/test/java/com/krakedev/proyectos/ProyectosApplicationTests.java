2619b2a57f7055770857b46205da6c786f4d11b6
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-13 16:20:45 -0500

    JJBA: Se comenta el codigo de la clase de test

--- src/test/java/com/krakedev/proyectos/ProyectosApplicationTests.java
+++ src/test/java/com/krakedev/proyectos/ProyectosApplicationTests.java
@@ -1,13 +1,13 @@
-package com.krakedev.proyectos;
+//package com.krakedev.proyectos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProyectosApplicationTests {
+//@SpringBootTest
+//class ProyectosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
