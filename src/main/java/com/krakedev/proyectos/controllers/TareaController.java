60833e875797933040ad13f4177717f7f8ea046b
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-13 18:11:38 -0500

    JJBA: Actualizada clase en pkg controller: TareaController.java - Se agregan anotaciones de @PreAuthorize en el metodo insertar como admin y en listar como user y admin

--- src/main/java/com/krakedev/proyectos/controllers/TareaController.java
+++ src/main/java/com/krakedev/proyectos/controllers/TareaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class TareaController {
 	private TareaService tService;
 	
 	@PostMapping
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> insertar(@RequestBody Tarea tarea) {
         try {
         	Tarea creado = tService.insertar(tarea);
@@ -37,6 +39,7 @@ public class TareaController {
 	}
 	
 	@GetMapping
+	@PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<?> listarTodos() {
 		try {
 			List<Tarea> Tareas =tService.listarTodos();
